3667a18f2d1a113cfffdfae3504501e57c11ff32
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-27 10:44:07 -0400

    add menu

--- portfolio-website/src/components/NavBar/NavBar.jsx
+++ portfolio-website/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './NavBar.css'
+import logo from '../../assets/logo.svg'
 
 const NavBar = () => {
   return (
-    <div className= 'navbar' > </div>
+    <div className= 'navbar'>
+        <img src={logo} alt="" />
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Photography</li>
+            <li>Contact</li>
+        </ul>
+    </div>
   )
 }
 
