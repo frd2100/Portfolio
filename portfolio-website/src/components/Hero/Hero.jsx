5c4b6d72e4bb6f37524d814d26f579786ea55c0e
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-29 13:11:36 -0400

    add style

--- portfolio-website/src/components/Hero/Hero.jsx
+++ portfolio-website/src/components/Hero/Hero.jsx
@@ -6,8 +6,11 @@ function Hero() {
   return (
     <div className="hero">
       <img src={profile_img} alt="" />
-      <h1>I'm Federico Parrinello, Full Stack developer Based in Toronto, Canada</h1>
-      <p></p>
+      <h1><span>I'm Federico Parrinello,</span> Full Stack developer Based in Toronto, Canada</h1>
+      <p>Originally from Italy, I bring three years of comprehensive experience as a Full Stack Developer, specializing in both front and back-end technologies.</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+      </div>
     </div>
   );
 }
