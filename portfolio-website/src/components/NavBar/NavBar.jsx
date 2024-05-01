79f3c17bb9124c5d906c3881f5114c7a34fcfefc
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-30 21:43:40 -0400

    Update NavBar.jsx

--- portfolio-website/src/components/NavBar/NavBar.jsx
+++ portfolio-website/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './NavBar.css'
-import logo from '../../assets/logo.svg'
 import ConnectWithMe from '../ConnectWithMe/ConnectWithMe.jsx'
+import logo from '../../assets/logo.svg'
+import underline from '../../assets/nav_underline.svg'
+
 
 const NavBar = () => {
+ const [menu, setmenu] = useState("home")
   return (
     <div className= 'navbar'>
         <img src={logo} alt="" />
         <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Photography</li>
-            <li>Contact</li>
+            <li><p>Home</p>{menu==="home"? <img src={underline} alt=''/>:<></> }</li>
+            <li><p>About Me</p>{menu==="about"? <img src={underline} alt=''/>:<></> }</li>
+            <li><p>Portfolio</p>{menu==="services"? <img src={underline} alt=''/>:<></> }</li>
+            <li><p>Photography</p>{menu===""? <img src={underline} alt=''/>:<></> }</li>
+            <li><p>Contact</p>{menu==="contact"? <img src={underline} alt=''/>:<></> }</li>
         </ul>
         <ConnectWithMe label='Connect with Me'/>
     </div>
