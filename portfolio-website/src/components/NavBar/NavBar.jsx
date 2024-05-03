d10348bc634d9b352bddcc318de92e52901a23f3
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-05-02 20:24:54 -0400

    add name on navbar

--- portfolio-website/src/components/NavBar/NavBar.jsx
+++ portfolio-website/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './NavBar.css'
 import ConnectWithMe from '../ConnectWithMe/ConnectWithMe.jsx'
-import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 
 
@@ -10,7 +9,7 @@ const NavBar = () => {
  const [menu, setMenu] = useState("home")
   return (
     <div className= 'navbar'>
-        <img src={logo} alt="" />
+        <h1>Federico</h1>
         <ul className="nav-menu">
             <li><AnchorLink className='anchor-link' href='#home'> <p onClick={()=> setMenu("home")}>Home</p></AnchorLink>{menu==="home"? <img src={underline} alt=''/>:<></> }</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'> <p onClick={()=> setMenu("about")}>About Me</p></AnchorLink>{menu==="about"? <img src={underline} alt=''/>:<></> }</li>
@@ -18,7 +17,7 @@ const NavBar = () => {
             <li><AnchorLink className='anchor-link' offset={50} href='#photography'><p onClick={()=> setMenu("photography")}>Photography</p></AnchorLink> {menu===""? <img src={underline} alt=''/>:<></> }</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=> setMenu("contact")}>Contact</p> </AnchorLink>{menu==="contact"? <img src={underline} alt=''/>:<></> }</li>
         </ul>
-        <AnchorLink className='anchor-link' offset={50} href='#contact'><ConnectWithMe label='Connect with Me'/></AnchorLink>
+        <ConnectWithMe label='Connect with Me'/><AnchorLink className='anchor-link' offset={50} href='#contact'></AnchorLink>
     </div>
   )
 }
